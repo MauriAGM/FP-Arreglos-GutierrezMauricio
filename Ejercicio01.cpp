897fc6d1e49a2b5cf9aa010ffeb43aff57ed7990
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-17 22:28:07 -0500

    Ejecicio 01 - Avance 1: Se implemento una funcion void para conversión de mayúsculas iniciales

---
+++ Ejercicio01.cpp
@@ -0,0 +1,21 @@
+//Convertir palabras a mayúsculas iniciales: "cáceres patrimonio de la humanidad" → "Cáceres Patrimonio De La Humanidad"
+#include <iostream>
+using namespace std;
+
+void convertirMayusIniciales(char cadena[]) {
+    int n = 0;
+    while (cadena[n] != '\0') {
+        if (n == 0 || cadena[n - 1] == ' ') {
+            cadena[n] = toupper(cadena[n]); 
+            //Toupper convierte a mayuscula la letra a menos que no sea una letra o ya esta en mayúscula 
+        }
+        n++;
+    }
+    cout << cadena << endl;
+}
+
+int main(){
+    char frase[] = "caceres patrimonio de la humanidad";
+    convertirMayusIniciales(frase);
+    return 0;
+}
