25528d6569769716ef5b8d6f231796f02a4ca404
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-19 12:01:06 -0500

    Se agrega el ejercicio 04 : Donde se subira el code

---
+++ Ejercicio04.cpp
@@ -0,0 +1,17 @@
+//Leer una secuencia de nombres, ordenarlos alfabéticamente y presentarlos. 
+
+#include <iostream>
+using namespace std;
+
+void leerNombres(){
+
+}
+
+void ordenarnombres() {
+
+}
+
+int main(){
+
+    return 0;
+}
