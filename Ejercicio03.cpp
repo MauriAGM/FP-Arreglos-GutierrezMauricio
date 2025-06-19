170ef4f0c68bc03f132163118e1895b0303205de
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-19 11:37:49 -0500

    Ejercicio 03 - Avance 1: Se implementaron las funciones void contarOcurrencias y leerCadena, y se creo la estructura base del programa

--- Ejercicio03.cpp
+++ Ejercicio03.cpp
@@ -3,10 +3,28 @@
 #include <iostream>
 using namespace std;
 
-void contarOcurrencias(){
+void contarOcurrencias(char cadena[], char letra, int &contadorLetras) {
+    int n = 0;
+    contadorLetras = 0;
+    while (cadena[n] != '\0') {
+        if (cadena[n] == letra) {
+            contadorLetras++;
+        }
+        n++;
+    }
+}
 
+void leerCadena(char cadena[]) {
+    cout << "Ingrese una frase: ";
+    cin.getline(cadena, 200); 
 }
 
 int main(){
-
+    char cadena[200], letra;
+    int contadorLetras = 0;
+    leerCadena(cadena);
+    cout << "Ingrese la letra a contar: ";
+    cin >> letra;
+    contarOcurrencias(cadena, letra, contadorLetras);
+    cout << "La letra '" << letra << "' aparece " << contadorLetras << " veces en la frase." << endl;
 }
