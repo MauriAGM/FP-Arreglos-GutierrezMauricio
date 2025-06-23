dc0ec7b67f9a1cdf74bd6a202b67d645e9a629d4
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-23 09:28:05 -0500

    Ejercicio 06 - Avance 1: Se implementa un programa que lee numeros e indica la suma de los indices pares e impares de un vector

--- Ejercicio06.cpp
+++ Ejercicio06.cpp
@@ -2,15 +2,34 @@
 #include <iostream>
 using namespace std;
 
-void leerVector() {
+const int MAX = 200;
 
+void leerVector(int vec[], int n) {
+    for (int i = 0; i < n ; i++){
+        cin >> vec[i];
+    }
 }
 
-void sumarIndicesParImpar() {
-
+void sumarIndicesParImpar(int vec[], int n, int &sumaPar, int &sumaImpar) {
+    sumaPar = 0;
+    sumaImpar = 0;
+    for (int i = 0; i < n; i++){
+        if (i % 2 == 0){
+            sumaPar = sumaPar + vec[i];
+        } else {
+            sumaImpar = sumaImpar + vec[i];
+        }
+    }
+    cout << "Suma de elementos en indices pares : " << sumaPar << endl;
+    cout << "Suma de elementos en indices impares : " << sumaImpar << endl;
 }
 
 int main() {
-
+    int vec[MAX];
+    int n;
+    cin >> n;
+    leerVector(vec, n);    
+    int sumaPar = 0, sumaImpar = 0;
+    sumarIndicesParImpar(vec, n, sumaPar, sumaImpar);
     return 0;
 }
