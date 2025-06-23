b9015219a783841fa377605fed4b2473abb2fa0b
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-23 08:39:16 -0500

    Ejercicio 05 - Avance 1: Se implementa un programa que lee n números, halla el mayor/menor y cuenta repeticiones

--- Ejercicio05.cpp
+++ Ejercicio05.cpp
@@ -2,7 +2,44 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+const int MAX = 200;
+
+void leerNnumeros(int cadena[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "Numero " << (i + 1) << ": ";
+        cin >> cadena[i];
+    }
+}
+
+void compararNumeros(int cadena[], int n) {
+    int mayor = cadena[0], menor = cadena[0];
+    int repiteMayor = 1, repiteMenor = 1;
+
+    for (int i = 1; i < n; i++) {
+        if (cadena[i] > mayor) {
+            mayor = cadena[i];
+            repiteMayor = 1;
+        } else if (cadena[i] == mayor) {
+            repiteMayor++;
+        }
+
+        if (cadena[i] < menor) {
+            menor = cadena[i];
+            repiteMenor = 1;
+        } else if (cadena[i] == menor) {
+            repiteMenor++;
+        }
+    }
+
+    cout << "Numero mayor: " << mayor << " (repite " << repiteMayor << " veces)";
+    cout << "Numero menor: " << menor << " (repite " << repiteMenor << " veces)";
+}
+
+int main() {
+    int cadena[MAX];
+    int n;
+    cin >> n;
+    leerNnumeros(cadena, n);
+    compararNumeros(cadena, n);
     return 0;
 }
