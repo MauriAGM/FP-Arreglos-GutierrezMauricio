6a1fece50c9f5e8997696ebb51e1e50bbe26b1de
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-23 11:01:49 -0500

    Ejercicio 08 - Avance 1: Se implementa un programa que indique si el numero es capicua ingresando cifra por cifra

--- Ejercicio08.cpp
+++ Ejercicio08.cpp
@@ -2,11 +2,31 @@
 #include <iostream>
 using namespace std;
 
-void capicua(){
+bool capicua(int vect[], int n){
+    for (int i = 0; i < n / 2; i++){
+        if (vect[i] != vect[n-1-i]){
+            return false;
+        }
+    }
+    return true;
+}
 
+void leerCifras(int vect[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "Cifra [" << i << "]: ";
+        cin >> vect[i];
+    }
 }
 
 int main() {
-
+    int n;
+    cin >> n;
+    int vect[n];
+    leerCifras(vect, n);
+    if (capicua(vect, n)) {
+        cout << "El numero es capicua.\n";
+    } else {
+        cout << "El numero no es capicua.\n";
+    }
     return 0;
 }
