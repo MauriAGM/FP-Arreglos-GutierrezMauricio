07abff963fae90cae1a402a48e7643699dec9864
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-23 10:07:21 -0500

    Ejercicio 07 - Avance 1: Se implementa un programa que invierta e imprime un vector

--- Ejercicio07.cpp
+++ Ejercicio07.cpp
@@ -2,16 +2,33 @@
 #include <iostream>
 using namespace std;
 
+const int MAX = 200; 
+
 void leerVector(int vec[], int n) {
     for (int i = 0; i < n ; i++){
         cin >> vec[i];
     }
 }
 
-void invertirImprimir(){
-
+void invertirImprimir(int vec[], int n){
+    int temp;
+    for (int i = 0; i < n / 2 ; i++ ){
+        temp = vec[i];
+        vec[i] = vec[n - 1 - i];
+        vec[n - 1 - i] = temp;
+    }
+    for (int i = 0; i < n; i++ ){
+        cout << "Elemento [" << i << "]: " << vec[i] << endl;
+    }
 }
 
 int main(){
-    
+    int vec[MAX];
+    int n; 
+    cin >> n; 
+
+    leerVector(vec, n);
+    invertirImprimir(vec, n);
+    return 0;
+
 }
