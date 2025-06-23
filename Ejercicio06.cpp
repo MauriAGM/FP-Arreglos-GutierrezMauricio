47d564082ede9e45a6cde84e3a472bfff4451f58
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-23 08:59:05 -0500

    Se agrega el ejercicio 06: Donde se subira el code

---
+++ Ejercicio06.cpp
@@ -0,0 +1,16 @@
+//Calcular la suma de componentes de indice par e impar en un vector
+#include <iostream>
+using namespace std;
+
+void leerVector() {
+
+}
+
+void sumarIndicesParImpar() {
+
+}
+
+int main() {
+
+    return 0;
+}
