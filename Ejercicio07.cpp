178aaffc2fbc9cd1ffa63d4f0c5459d8bff51ca6
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-23 09:39:09 -0500

    Se agrega el ejercicio 07.: Donde se subira el code

---
+++ Ejercicio07.cpp
@@ -0,0 +1,17 @@
+//Invertir un vector e imprimirlo 
+#include <iostream>
+using namespace std;
+
+void leerVector(int vec[], int n) {
+    for (int i = 0; i < n ; i++){
+        cin >> vec[i];
+    }
+}
+
+void invertirImprimir(){
+
+}
+
+int main(){
+    
+}
