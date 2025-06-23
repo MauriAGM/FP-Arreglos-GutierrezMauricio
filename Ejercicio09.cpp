edf1fca68e08083c891b33dd363717e5ceaa220f
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-23 11:35:27 -0500

    Ejercicio 09 - Avance 1: Se implementa un programa que rota los componentes de un vector

--- Ejercicio09.cpp
+++ Ejercicio09.cpp
@@ -2,14 +2,32 @@
 #include <iostream>
 using namespace std;
 
-void leerVector() {
-
+void leerVector(int vec[], int n) {
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
 }
 
-void rotarImprimir() {
-
+void rotarImprimir(int vec[], int n) {
+    int ultimo = vec[n - 1]; 
+    for (int i = n - 1; i > 0; i--) {
+        vec[i] = vec[i - 1];
+    }
+    vec[0] = ultimo; 
+    for (int i = 0; i < n; i++) {
+        cout << vec[i] << endl;
+    }
 }
 
 int main() {
+    int n;
+    cin >> n;
+    int vec[n];
+    if (n < 1) {
+        cout << "Error: cantidad inválida.\n";
+        return 1;
+    }
+    leerVector(vec, n);
+    rotarImprimir(vec, n);
     return 0;
 }
