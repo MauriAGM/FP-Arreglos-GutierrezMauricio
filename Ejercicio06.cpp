1eab81b17387dd8294b501e528c05d851c969c43
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-23 09:37:59 -0500

    Ejercicio 06 - Version Final: Se agrego un titulo agregado y validacion de rango para n

--- Ejercicio06.cpp
+++ Ejercicio06.cpp
@@ -6,6 +6,7 @@ const int MAX = 200;
 
 void leerVector(int vec[], int n) {
     for (int i = 0; i < n ; i++){
+        cout << "Elemento [" << i << "]: ";
         cin >> vec[i];
     }
 }
@@ -27,7 +28,14 @@ void sumarIndicesParImpar(int vec[], int n, int &sumaPar, int &sumaImpar) {
 int main() {
     int vec[MAX];
     int n;
+    cout << "===================================================" << endl; 
+    cout << "      SUMA DE COMPONENTES DE INDICES PAR/IMPAR     " << endl;
+    cout << "Cuantos elementos tendra el vector? (1 a " << MAX << "): ";
     cin >> n;
+    if (n < 1 || n > MAX) {
+        cout << "Error. Cantidad invalida." << endl;
+        return 1;
+    }
     leerVector(vec, n);    
     int sumaPar = 0, sumaImpar = 0;
     sumarIndicesParImpar(vec, n, sumaPar, sumaImpar);
